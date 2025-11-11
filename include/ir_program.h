3881aa42041dd2444502133c3884b1c12e6c4228
Geeeean <violoni.gianluca@gmail.com>
2025-11-11 19:44:21 +0100

    cfg in ir

--- include/ir_program.h
+++ include/ir_program.h
@@ -1,10 +1,12 @@
 #ifndef IR_PROGRAM
 #define IR_PROGRAM
 
+#include "cfg.h"
 #include "config.h"
 #include "ir_function.h"
 #include "method.h"
 
-IrFunction* ir_program_get_function(const Method* m, const Config* cfg);
+IrFunction* ir_program_get_function_ir(const Method* m, const Config* cfg);
+Cfg* ir_program_get_cfg(const Method* m, const Config* cfg);
 
 #endif
--- src/ir_program.c
+++ src/ir_program.c
@@ -1,3 +1,4 @@
+// TODO: handle delete (avoid mem leak)
 #include "ir_program.h"
 
 #include <string.h>
@@ -7,44 +8,112 @@ typedef struct IRItem IRItem;
 struct IRItem {
     char* method_id;
     IrFunction* ir_function;
+    Cfg* cfg;
     IRItem* next;
 };
 
 // to be freed
 IRItem* it_map = NULL;
 
-IrFunction* ir_program_get_function(const Method* m, const Config* cfg)
+static IrFunction* find_function(const char* id)
+{
+    IrFunction* result = NULL;
+    for (IRItem* it = it_map; it != NULL; it = it->next) {
+        if (strcmp(id, it->method_id) == 0) {
+            result = it->ir_function;
+            break;
+        }
+    }
+
+    return result;
+}
+
+static Cfg* find_cfg(const char* id)
+{
+    Cfg* result = NULL;
+    for (IRItem* it = it_map; it != NULL; it = it->next) {
+        if (strcmp(id, it->method_id) == 0) {
+            result = it->cfg;
+            break;
+        }
+    }
+
+    return result;
+}
+
+static IRItem* build_item(const Method* m, const Config* cfg)
+{
+    if (!m || !cfg) {
+        return NULL;
+    }
+    const char* id = method_get_id(m);
+    if (!id) {
+        return NULL;
+    }
+
+    IRItem* it = malloc(sizeof(IRItem));
+    if (!it) {
+        return NULL;
+    }
+
+    // TODO: check for success
+    it->method_id = strdup(method_get_id(m));
+    it->ir_function = ir_function_build(m, cfg);
+    it->cfg = cfg_build(it->ir_function);
+
+    it->next = it_map;
+
+    return it;
+}
+
+IrFunction* ir_program_get_function_ir(const Method* m, const Config* cfg)
 {
     const char* id = method_get_id(m);
 
     // First check: no lock (fast)
-    for (IRItem* it = it_map; it != NULL; it = it->next) {
-        if (strcmp(id, it->method_id) == 0) {
-            return it->ir_function;
-        }
+    IrFunction* result = find_function(id);
+    if (result) {
+        return result;
     }
-
-    IrFunction* result = NULL;
 
 #pragma omp critical(ir_program_map)
     {
         // Second check: with lock (avoid double creation)
-        for (IRItem* it = it_map; it != NULL; it = it->next) {
-            if (strcmp(id, it->method_id) == 0) {
-                result = it->ir_function;
-                break;
-            }
-        }
+        result = find_function(id);
 
         if (!result) {
-            IRItem* it = malloc(sizeof(IRItem));
-            it->method_id = strdup(id);
-            it->ir_function = ir_function_build(m, cfg);
+            IRItem* it = build_item(m, cfg);
+            if (it) {
+                it_map = it;
+                result = it->ir_function;
+            }
+        }
+    }
 
-            it->next = it_map;
-            it_map = it;
+    return result;
+}
 
-            result = it->ir_function;
+Cfg* ir_program_get_cfg(const Method* m, const Config* cfg)
+{
+    const char* id = method_get_id(m);
+
+    // First check: no lock (fast)
+    Cfg* result = find_cfg(id);
+    if (result) {
+        return result;
+    }
+
+#pragma omp critical(ir_program_map)
+    {
+        // Second check: with lock (avoid double creation)
+        result = find_cfg(id);
+
+        if (!result) {
+            IRItem* it = build_item(m, cfg);
+            if (it) {
+                it_map = it;
+                result = it->cfg;
+            }
         }
     }
 
